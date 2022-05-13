f9955af37b8c281a104cacf795709a40c67c8fde
Kunal <kunal20vijay00@gmail.com>
2022-05-13 14:02:40 +0530

    added projects

--- src/Project.js
+++ src/Project.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./App.css";
 import "./Project.css";
-import Project1 from "./img/portfolio-1.jpg";
-import Project2 from "./img/portfolio-2.jpg";
-import Project3 from "./img/portfolio-3.jpg";
-import Project4 from "./img/portfolio-4.jpg";
+import Project1 from "./img/project-01.png";
+import Project2 from "./img/project-02.png";
+import Project3 from "./img/project-03.png";
+import Project4 from "./img/project-04.png";
+import Project5 from "./img/project-05.png";
 
 function Project() {
     return (
@@ -25,8 +26,8 @@ function Project() {
                                 <div className="mask__effect"> </div>
                             </div>
                             <div className="project__meta absolute">
-                                <h5 className="project__text">TextUtils</h5>
-                                <h4 className="project__text">Text Formatter Utility in ReactJS</h4>
+                                <h5 className="project__text">My Portfolio</h5>
+                                <h4 className="project__text">Portfolio website in ReactJS</h4>
                                 <a href="#link1" className='project__btn'>View Details</a>
                             </div>
                         </div>
@@ -40,24 +41,9 @@ function Project() {
                                 <div className="mask__effect"> </div>
                             </div>
                             <div className="project__meta absolute">
-                                <h5 className="project__text">TextUtils</h5>
-                                <h4 className="project__text">Text Formatter Utility in ReactJS</h4>
-                                <a href="#link1" className='project__btn'>View Details</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col__3">
-                        <div className="project__box pointer relative">
-                            <div className="project__box__img pointer relative">
-                                <div className="project__img__box">
-                                    <img src={Project3} alt="Project-1-img" className='project__img' />
-                                </div>
-                                <div className="mask__effect"> </div>
-                            </div>
-                            <div className="project__meta absolute">
-                                <h5 className="project__text">TextUtils</h5>
-                                <h4 className="project__text">Text Formatter Utility in ReactJS</h4>
-                                <a href="#link1" className='project__btn'>View Details</a>
+                                <h5 className="project__text">GDSC site</h5>
+                                <h4 className="project__text">GDSC Club IIITB website</h4>
+                                <a href="https://github.com/Kunal-Vijay/GDSC-IIITB-Official-Site" className='project__btn'>View Details</a>
                             </div>
                         </div>
                     </div>
@@ -70,9 +56,9 @@ function Project() {
                                 <div className="mask__effect"> </div>
                             </div>
                             <div className="project__meta absolute">
-                                <h5 className="project__text">TextUtils</h5>
-                                <h4 className="project__text">Text Formatter Utility in ReactJS</h4>
-                                <a href="#link1" className='project__btn'>View Details</a>
+                                <h5 className="project__text">Magic Notes</h5>
+                                <h4 className="project__text">A website for taking e-notes</h4>
+                                <a href="https://github.com/Kunal-Vijay/Magic-Notes" className='project__btn'>View Details</a>
                             </div>
                         </div>
                     </div>
@@ -80,14 +66,14 @@ function Project() {
                         <div className="project__box pointer relative">
                             <div className="project__box__img pointer relative">
                                 <div className="project__img__box">
-                                    <img src={Project1} alt="Project-1-img" className='project__img' />
+                                    <img src={Project5} alt="Project-1-img" className='project__img' />
                                 </div>
                                 <div className="mask__effect"> </div>
                             </div>
                             <div className="project__meta absolute">
                                 <h5 className="project__text">TextUtils</h5>
                                 <h4 className="project__text">Text Formatter Utility in ReactJS</h4>
-                                <a href="#link1" className='project__btn'>View Details</a>
+                                <a href="https://github.com/Kunal-Vijay/TextUtils" className='project__btn'>View Details</a>
                             </div>
                         </div>
                     </div>
@@ -95,17 +81,18 @@ function Project() {
                         <div className="project__box pointer relative">
                             <div className="project__box__img pointer relative">
                                 <div className="project__img__box">
-                                    <img src={Project2} alt="Project-1-img" className='project__img' />
+                                    <img src={Project3} alt="Project-1-img" className='project__img' />
                                 </div>
                                 <div className="mask__effect"> </div>
                             </div>
                             <div className="project__meta absolute">
-                                <h5 className="project__text">TextUtils</h5>
-                                <h4 className="project__text">Text Formatter Utility in ReactJS</h4>
-                                <a href="#link1" className='project__btn'>View Details</a>
+                                <h5 className="project__text">Personal Portfolio</h5>
+                                <h4 className="project__text">Basic portfolio using HTML and CSS</h4>
+                                <a href="https://github.com/Kunal-Vijay/Baisc-Portfolio" className='project__btn'>View Details</a>
                             </div>
                         </div>
                     </div>
+                    
 
                 </div>
             </div>
